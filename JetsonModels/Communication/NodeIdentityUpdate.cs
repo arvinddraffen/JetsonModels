6b6aaa1b52388c173ce092953b40aeb57e488d7c
Arvind Draffen <arvinddraffen@gmail.com>
2020-04-15 13:42:25 -0500

    Added communication models for communication between nodes and master node in data collection

---
+++ JetsonModels/Communication/NodeIdentityUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace JetsonModels.Communication
+{
+    /// <summary>
+    /// <see cref="NodeIdentityUpdate"/> defines the message sent from nodes to update
+    /// node identity and environmental information.
+    /// </summary>
+    public class NodeIdentityUpdate
+    {
+        public int NodeId { get; set; }
+
+        public int ClusterId { get; set; }
+
+        public string NodeIPAddress { get; set; }
+
+        public int NodeUpTimeSeconds { get; set; }
+
+        public string OperatingSystem { get; set; }
+    }
+}
---
+++ JetsonModels/Communication/NodeUtilizationUpdate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetsonModels.Database;
+
+namespace JetsonModels.Communication
+{
+    /// <summary>
+    /// <see cref="NodeUtilizationUpdate"/> defines the message used to encapsulate utilization statistic
+    /// updates from each node within a cluster.
+    /// </summary>
+    public class NodeUtilizationUpdate
+    {
+        public int NodeId { get; set; }
+
+        public int ClusterId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the timestamp at which the statistics were obtained.
+        /// </summary>
+        public DateTime TimeStamp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of cores which constitute this <see cref="Node"/>.
+        /// </summary>
+        public virtual ICollection<CpuCore> Cores { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total memory available on this <see cref="Node"/>, expressed in megabytes.
+        /// </summary>
+        public uint MemoryAvailable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total memory used on this <see cref="Node"/>, expressed in megabytes.
+        /// </summary>
+        public uint MemoryUsed { get; set; }
+    }
+}
